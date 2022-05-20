9fd679affe059f40bdd50dfe5d02fd21f0a55c2b
Eric Hochwald <erichochwald@erics-mbp.myfiosgateway.com>
2022-05-20 14:06:04 -0400

    added ids to selections and wrote js pseudocode

---
+++ script.js
@@ -0,0 +1,18 @@
+searchEngine();
+
+function searchEngine () {
+    var searchBtn = document.getElementById("search-button");
+    var searchInput = document.getElementById("search-input");
+
+    searchBtn.addEventListener("click", function() {
+        // submit search query based on user input
+        
+        // if user doesn't select a format, make it a generic search
+
+        // else record the format the user selected 
+
+        // perform a query based on the format
+
+        // if query response is successful, go to search results page
+    });
+}
